5564d97053411907fd4bb86e3d36ae0ecf2edb61
hunnain <hunnainpashahgchgc@gmail.com>
2018-09-12 22:33:01 +0500

    NewsFeed Updated

--- Apps/Component/NewsFeed/NewsFeed.js
+++ Apps/Component/NewsFeed/NewsFeed.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
         position:'relative',
         width: wp('100%'),
         height: hp('100%'),
-        // backgroundColor:'#EDF1F5'
+        backgroundColor:'white'
        },
        arrowbtnContainer:{
         position:'relative',
@@ -135,7 +135,7 @@ feedpic: {
         marginLeft:wp("17%")
     },
     otherPost:{
-        marginTop:wp("4%")
+        marginTop:wp("6%")
     }
     })
 
